dcba08f19c23b5e462eccee7d6ff24ff1d8a8b7d
firestige <zzt.nbl@hotmail.com>
2025-11-14 15:57:48 +0800

    回退初始状态

---
+++ src/main/java/xyz/firestige/dto/NetworkEndpoint.java
@@ -0,0 +1,10 @@
+package xyz.firestige.dto;
+
+public class NetworkEndpoint {
+    private String key;
+    private String value;
+    private String sourceIp;
+    private String sourceDomain;
+    private String targetIp;
+    private String targetDomain;
+}
---
+++ src/main/java/xyz/firestige/dto/TenantDeployConfig.java
@@ -0,0 +1,114 @@
+package xyz.firestige.dto;
+
+import java.util.List;
+
+public class TenantDeployConfig {
+    private Long deployUnitId;
+    private Long deployUnitVersion;
+    private String deployUnitName;
+    private List<NetworkEndpoint> networkEndpoints;
+    private String nacosNameSpace;
+    private Boolean defaultFlag;
+    private String tenantId;
+    private String calledNumberRules;
+    private String trunkGroup;
+    private Long planId;
+    private Long planVersion;
+    private TenantDeployConfig sourceTenantDeployConfig;
+
+    public Long getDeployUnitId() {
+        return deployUnitId;
+    }
+
+    public void setDeployUnitId(Long deployUnitId) {
+        this.deployUnitId = deployUnitId;
+    }
+
+    public Long getDeployUnitVersion() {
+        return deployUnitVersion;
+    }
+
+    public void setDeployUnitVersion(Long deployUnitVersion) {
+        this.deployUnitVersion = deployUnitVersion;
+    }
+
+    public String getDeployUnitName() {
+        return deployUnitName;
+    }
+
+    public void setDeployUnitName(String deployUnitName) {
+        this.deployUnitName = deployUnitName;
+    }
+
+    public List<NetworkEndpoint> getNetworkEndpoints() {
+        return networkEndpoints;
+    }
+
+    public void setNetworkEndpoints(List<NetworkEndpoint> networkEndpoints) {
+        this.networkEndpoints = networkEndpoints;
+    }
+
+    public String getNacosNameSpace() {
+        return nacosNameSpace;
+    }
+
+    public void setNacosNameSpace(String nacosNameSpace) {
+        this.nacosNameSpace = nacosNameSpace;
+    }
+
+    public Boolean getDefaultFlag() {
+        return defaultFlag;
+    }
+
+    public void setDefaultFlag(Boolean defaultFlag) {
+        this.defaultFlag = defaultFlag;
+    }
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getCalledNumberRules() {
+        return calledNumberRules;
+    }
+
+    public void setCalledNumberRules(String calledNumberRules) {
+        this.calledNumberRules = calledNumberRules;
+    }
+
+    public String getTrunkGroup() {
+        return trunkGroup;
+    }
+
+    public void setTrunkGroup(String trunkGroup) {
+        this.trunkGroup = trunkGroup;
+    }
+
+    public Long getPlanId() {
+        return planId;
+    }
+
+    public void setPlanId(Long planId) {
+        this.planId = planId;
+    }
+
+    public Long getPlanVersion() {
+        return planVersion;
+    }
+
+    public void setPlanVersion(Long planVersion) {
+        this.planVersion = planVersion;
+    }
+
+    public TenantDeployConfig getSourceTenantDeployConfig() {
+        return sourceTenantDeployConfig;
+    }
+
+    public void setSourceTenantDeployConfig(TenantDeployConfig sourceTenantDeployConfig) {
+        this.sourceTenantDeployConfig = sourceTenantDeployConfig;
+    }
+}
